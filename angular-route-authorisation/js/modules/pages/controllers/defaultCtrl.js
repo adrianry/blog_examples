389dd3e9924b53b00774d1cb0ce0af7e1b944abf
Adrian <ryser@netdot.ch>
2015-02-13 21:21:32 +0100

    add debug infos

--- angular-route-authorisation/js/modules/pages/controllers/defaultCtrl.js
+++ angular-route-authorisation/js/modules/pages/controllers/defaultCtrl.js
@@ -3,7 +3,15 @@
 
     angular.module(jcs.modules.pages.name)
         .controller(jcs.modules.pages.controllers.default, [
-        function () {
-        }
+            '$scope',
+            '$location',
+            '$route',
+            '$routeParams',
+            function ($scope, $location, $route, $routeParams) {
+                $scope.adi = "Hallo Adi";
+                $scope.$location = $location;
+                $scope.$route = $route;
+                $scope.$routeParams = $routeParams;
+            }
     ]);
 }(angular, jcs));
