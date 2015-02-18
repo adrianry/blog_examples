6ffe6e5349b5d97460aab469fd3a8ba1bd4b3c10
adrianry <ryser@netdot.ch>
2015-02-18 09:10:11 +0100

    logout event trigger gebaut

--- angular-route-authorisation/js/modules/auth/controllers/logoutCtrl.js
+++ angular-route-authorisation/js/modules/auth/controllers/logoutCtrl.js
@@ -6,13 +6,21 @@
 
     angular.module(jcs.modules.auth.name).controller(jcs.modules.auth.controllers.logout, [
         '$scope',
+        '$rootScope',
         '$location',
         jcs.modules.auth.services.authentication,
-        function ($scope, $location, authentication) {
+        function ($scope, $rootScope, $location, authentication) {
+            //Event Listener
+            $rootScope.$on(jcs.modules.auth.events.userLoggedOut, function() {
+                alert('Sie wurden erfolgreich abgemeldet. Sie werden zur Startseite weitergeleitet.');
+            });
+
+            //Buttonsteuerung
             $scope.nologout = true;
             var currentuser = authentication.getCurrentLoginUser();
             $scope.currentuser = currentuser;
             if(currentuser !== undefined)$scope.nologout = false;
+            //Logout Funktion
             $scope.logout = function () {
                 authentication.logout();
                 $location.path(jcs.modules.pages.routes.home);
